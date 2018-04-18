2cc0be8f56773d851641ec244252b29478fdb1ff
concept-inversion <santosh.pandey2222@gmail.com>
2018-04-18 22:55:19 +0545

    crud Module Implemented with Json uploader

---
+++ crudController.py
@@ -0,0 +1,45 @@
+from databaseModule import DB_connect
+import json
+#generate query from user input
+class CRUD():
+    def __init__(self, *args, **kwargs):
+        # use static methods
+        self.db = DB_connect('mysql.db') 
+    
+    def Create(self,each):
+        
+        columns = ', '.join(each.keys())
+        placeholders = ':'+', :'.join(each.keys())
+        statement = 'INSERT INTO PEOPLE (%s) VALUES (%s)'% (columns, placeholders,)
+        data=self.db.Link.execute(statement,each)
+        data= self.db.executeDB(statement)   
+        return data
+
+    def Update(self,param):
+
+        statement= 'UPDATE PEOPLE SET %s'% (param)
+        print(statement)
+        data=self.db.executeDB(statement)
+        return data
+
+    def Read(self, param):
+        statement= ' SELECT * FROM PEOPLE WHERE (%s)'% (param)
+        data=self.db.executeDB(statement)
+        return data
+
+    def Delete(self, param):
+        statement= 'DELETE FROM PEOPLE WHERE (%s)'%(param)
+        data=self.db.executeDB(statement)
+        return data
+
+    def JsonLoader(self,data):
+        raw=json.load(open(data))
+        for each in raw:
+            columns = ', '.join(each.keys())
+            placeholders = ':'+', :'.join(each.keys())
+            statement = 'INSERT INTO PEOPLE (%s) VALUES (%s)'% (columns, placeholders,)
+            data=self.db.Link.execute(statement,each)
+        return data
+
+
+
