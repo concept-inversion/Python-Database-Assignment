510061d76e67ba184a045651e824a4b6264c6a42
concept-inversion <santosh.pandey2222@gmail.com>
2018-04-18 21:56:53 +0545

    execute method added

--- databaseModule.py
+++ databaseModule.py
@@ -1,48 +1,51 @@
 import sqlite3
 from sqlite3 import Error
 
+
 class DB_connect(object):
     def __init__(self, name):
         try:
             self.Link = sqlite3.connect(name)
             self.cursor = self.Link.cursor()
-            print(sqlite3.version)
+            self.createTable()
         except Error as err:
             print(err)   
 
     def executeDB(self,statement):
         try:
-            self.cursor.execute(statement)
+            data = self.cursor.execute(statement)
+            self.Link.commit()
+            return data
         except Error as err:
             print(err)
+        
             
         
     def createTable(self):
         statement = '''
-        CREATE TABLE IF NOT EXISTS PEOPLE
-        (ID INT PRIMARY KEY NOT NULL,
-        NAME TEXT NOT NULL,
-        EMAIL TEXT NOT NULL,
-        BIO TEXT,
-        GENDER TEXT,
-        LINK TEXT,
-        ADDRESS TEXT,
-        LONGITUDE REAL,
-        LATITUDE REAL,
-        IMAGE BLOB,
-        PHONE INT,
-        DOB TEXT
+ CREATE TABLE IF NOT EXISTS PEOPLE
+        (Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Name TEXT NOT NULL,
+        Email TEXT NOT NULL,
+        Bio TEXT,
+        Gender VARCHAR(12),
+        Link TEXT,
+        Address TEXT,
+        Longitude REAL,
+        Latitude REAL,
+        Image BLOB,
+        Phone INT,
+        Dob DATE
+
         );
-        '''
-        
+'''        
         try:
-            self.cursor.execute(statement)
+            data= self.cursor.execute(statement)
             self.Link.commit()
+            print("table creataed")
         except Error as err:
             print(err)
       
-        
-
     def closeDB(self):
         try:
             self.Link.commit()
@@ -50,10 +53,3 @@ class DB_connect(object):
             print('Database Closed Successfully')
         except Error as err:
             print(err)
-                
-
-if __name__== '__main__':
-
-    database = DB_connect('mysql.db')
-    database.createTable()
-    database.closeDB()
