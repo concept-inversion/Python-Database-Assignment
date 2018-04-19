649bc59a9f9257c24199147917f95d2fbe69352c
concept-inversion <santosh.pandey2222@gmail.com>
2018-04-19 14:33:05 +0545

    Insertion sort added key=name

--- SortnSearch.py
+++ SortnSearch.py
@@ -1,2 +1,33 @@
 from databaseModule import DB_connect
 from crudController import CRUD
+
+class sortSearch():
+    def __init__(self, *args, **kwargs):
+        self.crud= CRUD()
+
+    def Search(self):
+        #Search in every column
+        pass
+
+
+    def Sort(self,*args):
+        data = self.crud.View()
+        print("suce")
+        data = [x[1] for x in data]
+        self.InsertionSort(data)
+
+    def InsertionSort(self,data):
+        length = len(data)
+        for i in range(length):
+            Value = data[i]
+            holePos = i
+            while ((holePos >0) and (data[holePos-1] > Value)):
+                data[holePos] = data[holePos-1]
+                holePos= holePos-1        
+            data[holePos]=Value
+        print(data)
+        return data
+
+    def QuickSort(self):
+        pass
+
