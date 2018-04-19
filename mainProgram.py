742bd7bfb77cb30469cbd156dc9cfb1e3bfca564
concept-inversion <santosh.pandey2222@gmail.com>
2018-04-19 10:22:06 +0545

    A bit Interactive

--- mainProgram.py
+++ mainProgram.py
@@ -1,6 +1,8 @@
 import json
 from databaseModule import DB_connect
 from crudController import CRUD
+import dis
+import os
 
 
 class Program():
@@ -9,7 +11,7 @@ class Program():
 
     def Delete(self):
         # [Column name] [operation][cond]
-        cond = input("Enter the query starting from column name: ")
+        cond = input("[Delete]Enter the query starting from column name: ")
         self.crud.Delete(cond)
 
     def Insert(self):
@@ -30,31 +32,49 @@ class Program():
             format[key]= input("Enter the  "+key )
         self.crud.Create(format)
 
-    def Update(self,args):
+    def Update(self):
         # [Column name] [operation][cond]
-        cond = input("Enter the query starting from column name: ")
+        cond = input("[Update]Enter the query starting from column name: ")
         self.crud.Read(cond)
 
     def Select(self):
         # [Column name] [operation][cond]
-        cond = input("Enter the query starting from column name: ")
+        cond = input("[Select]Enter the query starting from column name: ")
+        print("read called")
         output = self.crud.Read(cond)
-        #print("output from database : " + output)
+        print("data retrieved")
+        print(output.fetchall())
+        if output:
+            for each in output:
+                print (each)
+
+        else:
+            print("no data")
         return output
     
-    def upload_json(self,data):
+    def upload_json(self):
+        data = input("Enter json file: ")
         self.crud.JsonLoader(data)
         
 
 if __name__== '__main__':
     new = Program()
-#new.Insert()
-    data=new.Select()
-    for each in data:
-        print (each)
-    '''
-    new.upload_json('raw_data.json')
-    data1=new.Update("Email = 'kale' WHERE Image= 'hans.io'")
-    data = new.Select("Email= 'kale'")
-    '''
+    options = [None,new.Insert,new.Select,new.Update,new.Delete,new.upload_json]
+    while True:
+        action = int(input(
+            '''
+            Press 1 to Insert
+            Press 2 to Select
+            Press 3 to Update
+            Press 4 to Delete
+            Press 5 to Load JSON
+            Press 0 to exit
+            '''
+        ))
+        if(action==0):
+            break
+        else:
+            options[action]()
+        
+
 
