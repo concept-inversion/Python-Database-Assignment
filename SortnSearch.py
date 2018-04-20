a2e57577bf22952aaca23a87e0a507a8e7ff4d51
concept-inversion <santosh.pandey2222@gmail.com>
2018-04-20 12:13:50 +0545

    Quick Sort Implemented

--- SortnSearch.py
+++ SortnSearch.py
@@ -1,6 +1,7 @@
+import statistics
 from databaseModule import DB_connect
 from crudController import CRUD
-
+import random
 class sortSearch():
     def __init__(self, *args, **kwargs):
         self.crud= CRUD()
@@ -14,7 +15,9 @@ class sortSearch():
         data = self.crud.View()
         print("suce")
         data = [x[1] for x in data]
-        self.InsertionSort(data)
+        #out = self.InsertionSort(data)
+        out = self.ThreeP_QuickSort(data)
+        return out
 
     def InsertionSort(self,data):
         length = len(data)
@@ -28,6 +31,37 @@ class sortSearch():
         print(data)
         return data
 
-    def QuickSort(self):
-        pass
+    def ThreeP_QuickSort(self,data):
+        
+        
+        def pivotFind(start,end):
+            if (end-start) > 3:
+                rand = random.sample(range(start,end),3)
+            else:
+                rand = random.sample(range(start,end),1)
+            x= [data[a] for a in rand]
+            median = statistics.median(x)
+            pivot = data.index(median)
+            return pivot
+        
+        def partitionFunc(left,right,pivot):
+            newPivotIndex = left -1    
+            for index in range(left,right):
+                if data[index]<data[right]:#check if current val is less than pivot value
+                    newPivotIndex=newPivotIndex+1
+                    data[newPivotIndex], data[index] = data[index],data[newPivotIndex]
+                    
+            data[newPivotIndex+1],data[right] = data[right] , data[newPivotIndex+1]
+            return newPivotIndex+1
 
+        def quick(start,end):
+            if start < end:
+                pivot = pivotFind(start,end) 
+                data[pivot],data[end]=data[end],data[pivot]
+                p = partitionFunc(start,end,pivot)
+                quick(start,p-1)
+                quick(p+1,end)
+        
+        length = len(data)
+        quick(0,length-1)
+        return data
